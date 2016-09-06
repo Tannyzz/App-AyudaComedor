782a4bce3697f73593b84995ecf3f67eb6d59a7a
Josue <josue.marfil@estudiante.uacm.edu.mx>
2016-09-06 16:04:47 -0500

    Se programo la rutina en la clase de java para mandar notificaciones al layout principal

--- app/src/main/java/com/mobauacm/naveli/ayudacomdeor/MainActivity.java
+++ app/src/main/java/com/mobauacm/naveli/ayudacomdeor/MainActivity.java
@@ -1,13 +1,31 @@
 package com.mobauacm.naveli.ayudacomdeor;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creacion la rutina para mostrar un dialogo antes de iniciar
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Escriba la Informacion a aceptar")
+                .setTitle("Información")
+                .setCancelable(false)
+                .setNeutralButton("Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+        //Termina la rutina de alerta
     }
 }
